ec4bec4ccf5bf8c37fb6b838a8ff4b7700caaacf
Clarence HIRSCH <chirsch1@myges.fr>
2025-02-15 23:39:04 +0100

    google cloud api deployment working

--- anki-remastered-api/src/config/database.config.ts
+++ anki-remastered-api/src/config/database.config.ts
@@ -7,15 +7,21 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
     constructor(private configService: ConfigService) {}
 
     createTypeOrmOptions(): TypeOrmModuleOptions {
+        const isProduction = process.env.NODE_ENV === 'production';
+
         return {
             type: 'postgres',
-            host: this.configService.get<string>('DB_HOST'),
-            port: this.configService.get<number>('DB_PORT'),
+            host: isProduction ? `/cloudsql/${this.configService.get<string>('DB_INSTANCE_CONNECTION_NAME')}`
+                : this.configService.get<string>('DB_HOST'),
+            port: isProduction ? undefined : this.configService.get<number>('DB_PORT'),
             username: this.configService.get<string>('DB_USERNAME'),
             password: this.configService.get<string>('DB_PASSWORD'),
             database: this.configService.get<string>('DB_DATABASE'),
             autoLoadEntities: true,
             synchronize: true,
+            extra: isProduction
+                ? { socketPath: `/cloudsql/${this.configService.get<string>('DB_INSTANCE_CONNECTION_NAME')}` }
+                : {},
         };
     }
 }
